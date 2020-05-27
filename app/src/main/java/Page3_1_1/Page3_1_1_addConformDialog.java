dd2ae81e45431ca494a01ef8cc881db3c4648c3d
choimunjeong <ctj123@naver.com>
2020-05-28 03:46:56 +0900

    page3_1_1 역 추가시 나오는 바텀시트 dismiss 버튼 구현 05/28 03:46

--- app/src/main/java/Page3_1_1/Page3_1_1_addConformDialog.java
+++ app/src/main/java/Page3_1_1/Page3_1_1_addConformDialog.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.hansol.spot_200510_hs.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 
@@ -53,6 +55,11 @@ public class Page3_1_1_addConformDialog extends BottomSheetDialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.page3_1_1_addconfirm, container, false);
 
+        //----------------------------여기추가
+        //바텀시트가 자동으로 내려가는 걸 방지
+        setCancelable(false);
+
+
         //액티비티에서 값을 전달 받음
         Bundle extra = getArguments();
         name = extra.getString("name");
@@ -71,6 +78,15 @@ public class Page3_1_1_addConformDialog extends BottomSheetDialogFragment {
             }
         });
 
+
+        //창 닫기 버튼을 누르면----------------------------------------------------여기추가
+        ImageView disappear_btn = rootview.findViewById(R.id.page3_1_1_disappearBtn);
+        disappear_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
         return rootview;
     }
 
