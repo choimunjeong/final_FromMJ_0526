b416af2807ce1b7f455b22fad6bb2b0df6e3a617
choimunjeong <ctj123@naver.com>
2020-05-28 13:21:05 +0900

    모든 Main 페이지에서 로고 누를때 화면 반짝거리는 거 해결 05/28 13:20

--- app/src/main/java/Page1/Page1_1_1.java
+++ app/src/main/java/Page1/Page1_1_1.java
@@ -123,10 +123,8 @@ public class Page1_1_1 extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Page1.class);
                 intent.addFlags(intent.FLAG_ACTIVITY_SINGLE_TOP);
                 intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP);
-                intent.addFlags(FLAG_ACTIVITY_NO_ANIMATION);
-                //overridePendingTransition(0,0);
                 startActivity(intent);
-
+                overridePendingTransition(0,0);  //순서를 바꿔줌
             }
         });
 
